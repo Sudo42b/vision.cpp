90aa0225698ca4458d9fb4872eeb84e0eff84646
Acly <aclysia@gmail.com>
2025-07-09 18:38:26 +0200

    project: update readme, merge conversion scripts into single file (so it can be copied and run anywhere)

--- README.md
+++ README.md
@@ -5,34 +5,37 @@ Computer Vision ML inference in C++
 * Self-contained C++ library
 * Efficient inference on consumer CPU and GPUs
 * Lightweight deployment on many platforms (Windows, Linux,)
-* Simple modular design for experimentation and extension
+* Modular design for experimentation and extension
 
-Follows the spirit of the popular [llama.cpp]() project and is based on the same [infrastructure]().
+Inspired by the [llama.cpp]() project and based on the same [infrastructure]().
+
+> [!NOTE]
+> The API is not considered stable at the moment and may change without notice.
 
 ### Features
 
-| Model         | Task             | Backends    |
-| :------------ | :--------------- | :---------- |
-| [MobileSAM]() | Segmentation     | CPU, Vulkan |
-| [BiRefNet]()  | Segmentation     | CPU, Vulkan |
-| [MI-GAN]()    | Inpainting       | CPU, Vulkan |
-| [ESRGAN]()    | Super-resolution | CPU, Vulkan |
+| Model                                        | Task             | Backends    |
+| :------------------------------------------- | :--------------- | :---------- |
+| [**MobileSAM**](#segment-anything-model-sam) | Segmentation     | CPU, Vulkan |
+| [**BiRefNet**](#birefnet)                    | Segmentation     | CPU, Vulkan |
+| [**MI-GAN**](#mi-gan)                        | Inpainting       | CPU, Vulkan |
+| [**ESRGAN**](#esrgan)                        | Super-resolution | CPU, Vulkan |
 
 ## Get Started
 
-See [Building]() to build from source.
+See [Building](#building) to build from source. Binaries can be found in `build/bin` afterwards.
 
 ### Example: Select an object in an image
 
 Let's use MobileSAM to generate a segmentation mask for the <object>
 at pixel position (320, 240).
 
-First, download the model from [huggingface]().
+You can download the required model from huggingface: [MobileSAM-F16.gguf]().
 
 #### CLI
 
-```
-vision-cli -m MobileSAM-F16.gguf -i input.png -p "[320, 240]" -o mask.png
+```sh
+vision-cli -m MobileSAM-F16.gguf -i input.png -p 320 240 -o mask.png
 ```
 
 #### API
@@ -42,52 +45,86 @@ vision-cli -m MobileSAM-F16.gguf -i input.png -p "[320, 240]" -o mask.png
 using namespace visp;
 
 void main() {
-  backend cpu     = init_backend(backend_type::cpu);
-  sam_model model = load_sam_model(cpu, "MobileSAM-F16.gguf");
+  backend   cpu = backend_init(backend_type::cpu);
+  sam_model sam = sam_load_model("MobileSAM-F16.gguf", cpu);
   
-  image_data input     = load_image("input.png");
-  sam_embedding embeds = embed(cpu, model, input);
-  image_data mask      = predict(cpu, model, embeds, point{320, 240});
-  save_image(mask, "mask.png");
+  image_data input_image = image_load("input.png");
+  sam_encode(sam, input_image, cpu);
+
+  image_data object_mask = sam_compute(sam, {320, 240}, cpu);
+  image_save(object_mask, "mask.png");
 }
 ```
-There is also a low-level API with a high degree of flexibility and control over
-allocations and performance.
-
-> ![WARNING]
-> The API is not considered stable at the moment and may change without notice.
+This shows the high-level API. Internally it is composed of multiple smaller
+functions that handle model loading, pre-processing inputs, transferring
+data to backend devices, post-processing output, etc. 
+These can be used as building blocks for flexible pipelines which integrate
+with your existing data sources and infrastructure.
 
 #### UI
 
 
 ## Models
 
+### Segment Anything Model (SAM)
+
+```sh
+vision-cli sam -m models/MobileSAM.gguf -i input.png -p 300 200 -o mask.png --composite comp.png
+```
+
+### BiRefNet
+
+```sh
+vision-cli birefnet -m models/BiRefNet_lite-F16.gguf -i input.png -o mask.png --composite comp.png
+```
+
+### MI-GAN
+
+```sh
+vision-cli migan -m models/migan_places2_512-F16.gguf -i image.png mask.png -o output.png
+```
+
+### ESRGAN
+
+```sh
+vision-cli esrgan -m models/4x_foolhardy_Remacrih-F16.gguf -i input.png -o output.png
+```
 
 
 ### Converting models
 
+Models need to be converted to GGUF before they can be used. This can also
+rearrange or precompute tensors for more optimal inference.
+
+To convert eg. an ESRGAN model, install [uv](https://docs.astral.sh/uv/) and run:
+```sh
+uv run scripts/convert.py esrgan 4x_NMKD-Superscale-SP_178000_G.pth -q f16
+```
+This will create `models/4x_NMKD-Superscale-SP_178000_G-F16.gguf`.
+
+See `convert.py --help` for more options.
 
 ## Building
 
 Building requires CMake and a compiler with C++20 support.
 
 **Get the sources**
-```
+```sh
 git clone --recursive
 cd vision.cpp
 ```
 
 **Configure and build**
-```
+```sh
 cmake . -B build
 cmake --build build --config Release
 ```
 
 ### Vulkan
 
-Vulkan GPU support requires the [Vulkan SDK]() to be installed.
+Vulkan GPU support requires the [Vulkan SDK](https://www.lunarg.com/vulkan-sdk/) to be installed.
 
-```
+```sh
 cmake . -B build -DVISP_VULKAN=ON
 cmake --build build --config Release
 ```
@@ -95,12 +132,12 @@ cmake --build build --config Release
 ### Tests
 
 Run all tests with the following command:
-```
+```sh
 ctest build -C Release
 ```
 
-Some tests require a Python environment. It can be set up with [uv]():
-```
+Some tests require a Python environment. It can be set up with [uv](https://docs.astral.sh/uv/):
+```sh
 # Setup venv and install dependencies
 uv sync
 
--- scripts/convert.py
+++ scripts/convert.py
@@ -1,12 +1,234 @@
 #!/usr/bin/env -S uv run --script
-
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#   "torch",
+#   "torchvision",
+#   "timm",
+#   "pytest",
+#   "opencv-python",
+#   "ruff",
+#   "einops>=0.8.1",
+#   "spandrel>=0.4.1",
+#   "gguf>=0.17.1",
+# ]
+# ///
 import argparse
-import sys
+import itertools
+import torch
+import safetensors
+import numpy as np
 
 from pathlib import Path
+from gguf import GGUFWriter, Metadata, GGML_QUANT_VERSION
+from torch import Tensor
 
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-from converters import sam, birefnet, migan, esrgan
+#
+# Common
+
+batch_norm_eps = 1e-5
+
+
+def is_conv_2d(name: str, tensor: Tensor):
+    return (
+        tensor.ndim == 4
+        and tensor.shape[2] == tensor.shape[3]
+        and tensor.shape[2] in (1, 3, 4, 7)
+        and name.endswith("weight")
+    )
+
+
+def conv_2d_to_nhwc(kernel: Tensor):
+    c_in = kernel.shape[1]
+    if c_in == 1:  # depthwise
+        return kernel.permute(2, 3, 1, 0)  # H W 1 C_out
+    else:
+        return kernel.permute(0, 2, 3, 1)  # C_out H W C_in
+
+
+def conv_transpose_2d_to_nhwc(kernel: Tensor):
+    # C_in C_out H W -> C_out H W C_in
+    return kernel.permute(1, 2, 3, 0)
+
+
+def add_tensor(
+    writer: GGUFWriter, name: str, tensor: Tensor, quantize: str, verbose: bool
+):
+    if len(name) >= 64:
+        print("Warning: name too long", len(name), name)
+
+    if quantize == "f16" and tensor.dtype == torch.float32:
+        tensor = tensor.to(torch.float16)
+
+    tensor_data = tensor.numpy()
+    if verbose:
+        print(name, tensor.shape, tensor_data.dtype)
+    writer.add_tensor(name, tensor_data)
+
+
+#
+# MobileSAM
+
+
+def convert_sam(
+    input_filepath: Path, writer: GGUFWriter, quantize: str | None, verbose: bool
+):
+    assert quantize is None, "MobileSAM does not support quantization"
+
+    model: dict[str, Tensor] = torch.load(
+        input_filepath, map_location="cpu", weights_only=True
+    )
+
+    for name, tensor in model.items():
+        name = name.replace("image_encoder.", "enc.")
+        name = name.replace("mask_decoder.", "dec.")
+        name = name.replace("_image_to_token.", "_i2t.")
+        name = name.replace("_token_to_image.", "_t2i.")
+
+        if name.endswith("attention_biases"):
+            num_heads = tensor.shape[0]
+            resolution = {4: 7, 5: 14, 10: 7}[num_heads]
+            attention_bias_idxs = build_attention_bias_indices(resolution)
+            name = name + "_indexed"
+            tensor = tensor[:, attention_bias_idxs]
+
+        if name.endswith("running_var"):
+            tensor = torch.sqrt(tensor + batch_norm_eps)
+
+        if (
+            name.endswith("c.weight")
+            or name.endswith("neck.0.weight")
+            or name.endswith("neck.2.weight")
+        ):
+            assert tensor.shape[2] == tensor.shape[3] and tensor.shape[2] <= 3
+            tensor = conv_2d_to_nhwc(tensor)
+
+        # Precompute dense positional embeddings from random matrix stored in the model
+        if name == "prompt_encoder.pe_layer.positional_encoding_gaussian_matrix":
+            pe = build_dense_positional_embeddings(tensor)
+            writer.add_tensor("dec.dense_positional_embedding", pe.numpy())
+
+        add_tensor(writer, name, tensor, quantize, verbose)
+
+
+def build_attention_bias_indices(resolution: int):
+    points = list(itertools.product(range(resolution), range(resolution)))
+    N = len(points)
+    attention_offsets = {}
+    idxs = []
+    for p1 in points:
+        for p2 in points:
+            offset = (abs(p1[0] - p2[0]), abs(p1[1] - p2[1]))
+            if offset not in attention_offsets:
+                attention_offsets[offset] = len(attention_offsets)
+            idxs.append(attention_offsets[offset])
+
+    return torch.LongTensor(idxs).view(N, N)
+
+
+def build_dense_positional_embeddings(
+    positional_encoding_gaussian_matrix: torch.Tensor, image_embedding_size=64
+):
+    # from sam/modeling/prompt_encoder.py - PositionEmbeddingRandom
+    h, w = image_embedding_size, image_embedding_size
+    grid = torch.ones((h, w), dtype=torch.float32)
+    y_embed = grid.cumsum(dim=0) - 0.5
+    x_embed = grid.cumsum(dim=1) - 0.5
+    y_embed = y_embed / h
+    x_embed = x_embed / w
+
+    coords = torch.stack((x_embed, y_embed), dim=-1)
+    coords = 2 * coords - 1
+    coords = coords @ positional_encoding_gaussian_matrix
+    coords = 2 * np.pi * coords
+    # outputs d_1 x ... x d_n x C shape
+    pe = torch.cat([torch.sin(coords), torch.cos(coords)], dim=-1)
+    return pe
+
+
+#
+# BirefNet
+
+
+def convert_birefnet(
+    input_filepath: Path, writer: GGUFWriter, quantize: str | None, verbose: bool
+):
+    model: dict[str, Tensor] = safetensors.safe_open(input_filepath, "pt")
+
+    for name in model.keys():
+        tensor = model.get_tensor(name)
+
+        # Shorten some names to fit into 64 chars
+        name = name.replace("decoder_block", "block")
+        name = name.replace("atrous_conv", "conv")
+        name = name.replace("modulator_conv", "modulator")
+        name = name.replace("offset_conv", "offset")
+        name = name.replace("regular_conv", "conv")
+
+        if name.endswith("relative_position_index"):
+            continue  # precomputed in c++ code
+
+        # BatchNorm2d: precompute sqrt(var + eps)
+        if name.endswith("running_var"):
+            tensor = torch.sqrt(tensor + batch_norm_eps)
+
+        if is_conv_2d(name, tensor):
+            tensor = conv_2d_to_nhwc(tensor)
+
+        add_tensor(writer, name, tensor, quantize, verbose)
+
+
+#
+# MI-GAN
+
+
+def convert_migan(
+    input_filepath: Path, writer: GGUFWriter, quantize: str | None, verbose: bool
+):
+    model: dict[str, Tensor] = torch.load(input_filepath, weights_only=True)
+
+    for name, tensor in model.items():
+        if is_conv_2d(name, tensor):
+            tensor = conv_2d_to_nhwc(tensor)
+
+        add_tensor(writer, name, tensor, quantize, verbose)
+
+
+#
+# ESRGAN
+
+
+def convert_esrgan(
+    input_filepath: Path, writer: GGUFWriter, quantize: str | None, verbose: bool
+):
+    from spandrel import ModelLoader
+
+    # Load the model using spandrel
+    # - it converts the various versions of ESRGAN checkpoints to a common format
+    model = ModelLoader().load_from_file(input_filepath)
+
+    if model.model.shuffle_factor is not None:
+        raise ValueError("RealESRGAN models with pixel shuffle are not supported yet.")
+    if getattr(model.model, "plus", False):
+        raise ValueError("RealESRGAN+ (plus) models are not supported yet.")
+
+    for name, tensor in model.model.state_dict().items():
+        if is_conv_2d(name, tensor):
+            tensor = conv_2d_to_nhwc(tensor)
+
+        add_tensor(writer, name, tensor, quantize, verbose)
+
+
+#
+# Main
+#######
+
+arch_names = {
+    "sam": "mobile-sam",
+    "birefnet": "birefnet",
+    "migan": "migan",
+    "esrgan": "esrgan",
+}
 
 if __name__ == "__main__":
     # fmt: off
@@ -16,6 +238,8 @@ if __name__ == "__main__":
     parser.add_argument("--output", "-o", type=str, default="models", help="Path to the output directory or file")
     parser.add_argument("--quantize", "-q", choices=["f16"], default=None, help="Convert float weights to the specified data type")
     parser.add_argument("--verbose", "-v", action="store_true", help="Enable verbose output")
+    parser.add_argument("--model-name", type=str, default=None, help="Name of the model for metadata")
+    parser.add_argument("--metadata", type=Path, help="Specify the path for an authorship metadata override file")
     # fmt: on
     args = parser.parse_args()
 
@@ -30,17 +254,27 @@ if __name__ == "__main__":
     print("* output:", output_path)
 
     try:
+        writer = GGUFWriter(output_path, arch_names.get(args.arch, args.arch))
+        metadata = Metadata.load(args.metadata, input_path.with_suffix(""), args.model_name)
+
         match args.arch:
             case "sam":
-                sam.convert(input_path, output_path, args.quantize, args.verbose)
+                convert_sam(input_path, writer, args.quantize, args.verbose)
             case "birefnet":
-                birefnet.convert(input_path, output_path, args.quantize, args.verbose)
+                convert_birefnet(input_path, writer, args.quantize, args.verbose)
             case "migan":
-                migan.convert(input_path, output_path, args.quantize, args.verbose)
+                convert_migan(input_path, writer, args.quantize, args.verbose)
             case "esrgan":
-                esrgan.convert(input_path, output_path, args.quantize, args.verbose)
+                convert_esrgan(input_path, writer, args.quantize, args.verbose)
             case _:
                 raise ValueError(f"Unknown architecture: {args.arch}")
+
+        metadata.set_gguf_meta_model(writer)
+        writer.add_quantization_version(GGML_QUANT_VERSION)
+        writer.write_header_to_file()
+        writer.write_kv_data_to_file()
+        writer.write_tensors_to_file(progress=True)
+        writer.close()
     except ValueError as e:
         print(e)
         exit(1)
