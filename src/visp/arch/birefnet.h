b22976cf7144fd14d55e1f5909294220cd73f458
sudo42b <x21999@naver.com>
2025-10-17 10:55:43 +0900

    confilect solved

--- src/visp/arch/birefnet.h
+++ src/visp/arch/birefnet.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 
 #include "visp/ml.h"
@@ -67,32 +66,3 @@ tensor decode(model_ref m, tensor x, swin_result const& features);
 
 } // namespace birefnet
 } // namespace visp
-=======
-#pragma once
-
-#include "visp/image.h"
-#include "visp/ml.h"
-#include "visp/vision.h"
-
-namespace visp::birefnet {
-
-// Encoder
-
-swin_result encode_concat(model_ref m, swin_result& xs, swin_result& xs_low);
-swin_result encode(model_ref m, tensor x, swin_params const& p);
-
-// Decoder
-
-tensor deformable_conv_2d(model_ref m, tensor x, int stride = 1, int pad = 0);
-tensor mean_2d(model_ref m, tensor x);
-tensor global_avg_pool(model_ref m, tensor x);
-tensor aspp_module_deformable(model_ref m, tensor x, int padding = 0);
-tensor aspp_deformable(model_ref m, tensor x);
-tensor basic_decoder_block(model_ref m, tensor x);
-tensor simple_conv(model_ref m, tensor x);
-tensor image_to_patches(model_ref m, tensor x, int64_t out_w, int64_t out_h);
-tensor gdt_conv(model_ref m, tensor x);
-tensor decode(model_ref m, tensor x, swin_result const& features);
-
-} // namespace visp::birefnet
->>>>>>> d381eaf731c1146f736e7e8da673a234deaa4d41
